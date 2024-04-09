5454883273ad0dce7ec120244af96076e9b0fcc3
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-04-09 22:56:01 +0300

    add ProductDetailView, ProductCreateView, ContactsView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from catalog.views import contacts, product_item, IndexView, ProductListView
+from catalog.views import ProductDetailView, IndexView, ProductListView, ProductCreateView, ContactsView
 
 app_name = 'catalog'
+
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('product_list/', ProductListView.as_view(), name='products'),
-    path('contacts/', contacts, name='contacts'),
-    path('product_item/<int:pk>', product_item, name='product_item')
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product_detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('product_create/', ProductCreateView.as_view(), name='product_create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
 from catalog.models import Product, OurContact
 
 
 class IndexView(TemplateView):
-    template_name = 'catalog/home.html'
+    template_name = 'catalog/product_list.html'
     extra_context = {'title': "Главная страница"}
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
         context_data['object_list'] = Product.objects.order_by("-id")[0:5]
         return context_data
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'category', 'price')
+    success_url = reverse_lazy("catalog:products")
 
 
 class ProductListView(ListView):
@@ -23,22 +29,18 @@ class ProductListView(ListView):
         context_data['object_list'] = Product.objects.order_by("-id")
         return context_data
 
-# def home(request):
-#     return render(request, 'catalog/home.html', {'title': "Главная страница",
-#                   'object_list': Product.objects.order_by("-id")[0:5]})
+
+class ProductDetailView(DetailView):
+    model = Product
+    extra_context = {'title': "Товар"}
 
 
-def contacts(request):
-    if request.method == 'POST':
-        # в переменной request хранится информация о методе, который отправлял пользователь
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        # а также передается информация, которую заполнил пользователь
+class ContactsView(TemplateView):
+    model = OurContact
+    template_name = 'catalog/contacts.html'
+    extra_context = {'title': "Контакты"}
 
-    return render(request, 'catalog/contacts.html', {'title': "Контакты", 'object': OurContact.objects.first()})
-
-
-def product_item(request, pk):
-    return render(request, 'catalog/product_item.html', {'title': "Товар",
-                  'object': Product.objects.get(pk=pk)})
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object'] = OurContact.objects.all()[0]
+        return context_data
