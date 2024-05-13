2f9f8b2dd94f5b08bf7909dbd9d7e284bfd3e29e
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-05-14 00:16:07 +0300

    add catalog\services.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.views import ProductDetailView, IndexView, ProductListView, ProductCreateView, ContactsView, \
     ProductUpdateView, ProductDeleteView, VersionListView, VersionCreateView, VersionUpdateView, VersionDetailView, \
-    VersionDeleteView, ProductUpdateModeratorView
+    VersionDeleteView, ProductUpdateModeratorView, CategoryListView
 
 app_name = 'catalog'
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
-    path('product_list/', ProductListView.as_view(), name='products'),
-    path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product_detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('product_list/', cache_page(60)(ProductListView.as_view()), name='products'),
+    path('category_list/', CategoryListView.as_view(), name='categories'),
+    path('contacts/', cache_page(6000)(ContactsView.as_view()), name='contacts'),
+    path('product_detail/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('product_create/', ProductCreateView.as_view(), name='product_create'),
     path('product_update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
     path('product_update_moderator/<int:pk>', ProductUpdateModeratorView.as_view(), name='product_update_moderator'),
