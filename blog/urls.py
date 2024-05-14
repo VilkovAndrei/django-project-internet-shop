8dc96947b84f5d0ed2314ae283bcbf49e26efb36
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-05-14 20:19:19 +0300

    edit requirements.txt

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from blog.views import PostListView, PostCreateView, PostUpdateView, PostDetailView, PostDeleteView
 
@@ -6,9 +7,9 @@ app_name = 'blog'
 
 
 urlpatterns = [
-    path('post_list/', PostListView.as_view(), name='post_list'),
+    path('post_list/', never_cache(PostListView.as_view()), name='post_list'),
     path('post_detail/<int:pk>', PostDetailView.as_view(), name='post_detail'),
-    path('post_create/', PostCreateView.as_view(), name='post_create'),
-    path('post_update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+    path('post_create/', never_cache(PostCreateView.as_view()), name='post_create'),
+    path('post_update/<int:pk>', never_cache(PostUpdateView.as_view()), name='post_update'),
     path('post_delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
 ]
